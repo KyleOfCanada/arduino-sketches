d9a57c02599fdb2583bc57cba0acbfa29237d929
Kyle Roddick <76850901+KyleOfCanada@users.noreply.github.com>
2023-11-14 15:50:47 -0400

    add temp sensor

---
+++ sketches/temp-sensor.ino
@@ -0,0 +1,17 @@
+int readPin = A2;
+float scaleF = 0.01;
+float temp = 1.;
+float readVal;
+float calVal;
+
+void setup() {
+    pinMode(readPin, INPUT);
+    Serial.begin(9600);
+}
+
+void loop() {
+    readVal = analogRead(readPin);
+    calVal = readVal * (5.0/1023.);
+    temp = calVal / scaleF;
+    Serial.println(temp);
+}
