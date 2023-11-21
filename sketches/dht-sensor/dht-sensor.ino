976476136eee1d74df02ea365f90a1b1bf9d245a
Kyle Roddick <76850901+KyleOfCanada@users.noreply.github.com>
2023-11-21 11:04:33 -0400

    finish dht-sensor

--- sketches/dht-sensor/dht-sensor.ino
+++ sketches/dht-sensor/dht-sensor.ino
@@ -1,14 +1,34 @@
 #include <DHT_U.h>
 #include <DHT.h>
-#define Type DHT11;
+#define Type DHT11
 
+int sense_pin = 2;
+int delay_time = 500;
 
-int read_pin = 2;
+DHT HT(sense_pin, Type);
+float humidity;
+float tempC;
+float tempF;
+float tempK;
 
 void setup() {
-
+  Serial.begin(9600);
+  HT.begin();
 }
 
 void loop() {
+  humidity = HT.readHumidity();
+  tempC = HT.readTemperature();
+  tempF = (tempC * 9. / 5.) + 32.;
+  tempK = tempC + 273.;
 
+  Serial.print("tempC: ");
+  Serial.println(tempC);
+  Serial.print("tempF: ");
+  Serial.println(tempF);
+  Serial.print("tempK: ");
+  Serial.println(tempK);
+  Serial.print("humidity: ");
+  Serial.println(humidity);
+  delay(delay_time);
 }
